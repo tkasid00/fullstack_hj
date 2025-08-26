5b2f61c69fc59045b7f2481d286db03a0939a758
tkasid00 <tkasid@gmail.com>
2025-08-26 17:41:37 +0900

    day002 정리

---
+++ track003_java/java2025/src/com/company/java001_ex/A000_ex.java
@@ -0,0 +1,12 @@
+package com.company.java001_ex;		//경로 위치
+
+//> Q) 패키지명 : com.company.java001_ex
+//>    클래스명 : A000_ex.java
+//>    "한 번 작성하면 어디서든 실행된다." 출력
+
+public class A000_ex {	//아무데서나 접근 가능, 부품 객체
+	public static void main(String[] args) {
+		System.out.println("한 번 작성하면 어디서든 실행된다.");
+	}
+
+}
