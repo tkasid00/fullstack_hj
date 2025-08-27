612d015c44fe34b1aa953ec1f3ea9ae46055f8ea
tkasid00 <tkasid@gmail.com>
2025-08-27 18:01:22 +0900

    0827정리

---
+++ track003_java/java2025/src/com/company/java001/A001.java
@@ -0,0 +1,11 @@
+package com.company.java001;
+
+public class A001 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+	} //end main
+	
+
+} //end class
+
---
+++ track003_java/java2025/src/com/company/java001_ex/A001_ex.java
@@ -0,0 +1,8 @@
+package com.company.java001_ex;
+
+public class A001_ex {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
