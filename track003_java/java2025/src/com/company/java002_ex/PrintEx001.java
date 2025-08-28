3665667a85261dde46511fbcfc2e56f09288d19b
tkasid00 <tkasid@gmail.com>
2025-08-28 17:36:46 +0900

    08281730

---
+++ track003_java/java2025/src/com/company/java002_ex/PrintEx001.java
@@ -0,0 +1,11 @@
+package com.company.java002_ex;
+
+public class PrintEx001 {
+	public static void main(String[] args) {
+		
+		System.out.println("좋아하는 색상은 RED입니다.");
+		System.out.print("좋아하는 색상은 RED입니다.\n");
+		System.out.printf("좋아하는 색상은 %s입니다.", "RED");
+	}
+
+}
---
+++ track003_java/java2025/src/com/company/java002_ex/VarEx001.java
@@ -0,0 +1,14 @@
+package com.company.java002_ex;
+
+public class VarEx001 {
+	public static void main(String[] args) {
+		
+		int apple;
+		apple=1000;
+		System.out.println("사과 가격은" + apple + "원입니다.");
+		
+		apple=2000;
+		System.out.println("사과 가격은" + apple + "원입니다.");
+	}
+
+}
