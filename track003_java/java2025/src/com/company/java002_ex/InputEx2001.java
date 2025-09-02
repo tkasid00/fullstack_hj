c956c9ca8557f598104c0f5a272608321046b10d
tkasid00 <tkasid00@gmail.com>
2025-09-02 18:08:57 +0900

    0902

--- track003_java/java2025/src/com/company/java002_ex/InputEx2001.java
+++ track003_java/java2025/src/com/company/java002_ex/InputEx2001.java
@@ -6,13 +6,22 @@ public class InputEx2001 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("파이값을 입력하시오> ");
-		double d = 3.14;
+		double d = 0.0; //초기값
 		
 		d = scanner.nextDouble();
 		
 		System.out.println("파이값은 " + d + "입니다.");
 		
 		
+		
+//		
+//		연습문제1)
+//		패키지명 : com.company.java002_ex
+//		클래스명 : InputEx2001
+//		출력내용 :  Scanner이용해서 파이값을 입력받고 출력하시오. 
+//		     파이값을 입력하시오 > _입력받기    3.141592    ( 자료형선택 )
+//		     파이값은 **입니다.
+//		
 		
 	}
 
